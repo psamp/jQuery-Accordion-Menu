cdb8f0546b7c81d83a9f8c8494e01a0ead6abcf8
Princess Sampson <ps@kingonomics.com>
2015-05-20 09:10:52 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-var para1 = $("var box = '<div id=""></div>';");
-var para2 = $("#btntwocontent");
-var para3 = $("#btnthreecontent");
 
-$("#btnone").on("click", function () {
 
-  para.toggleClass("hide");
-  para2.addClass("hide");
-  para2.addClass("hide");
-});
-
-$("#btntwo").on("click", function () {
-
-  para.addClass("show");
-});
-
-$("#btnthree").on("click", function () {
-
-  para.addClass("show");
-});
+$("section").on("click", function () {
+  $(".content").removeClass("show");
+  $(this).find($(".content")).addClass("show");
+})
