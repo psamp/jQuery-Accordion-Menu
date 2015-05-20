0e5775e438193b940f8e1cbc0b0a0ac1dddb0565
Princess Sampson <ps@kingonomics.com>
2015-05-20 07:07:29 -0400

    css is acting up

---
+++ main.js
@@ -0,0 +1,20 @@
+var para1 = $("var box = '<div id=""></div>';");
+var para2 = $("#btntwocontent");
+var para3 = $("#btnthreecontent");
+
+$("#btnone").on("click", function () {
+
+  para.toggleClass("hide");
+  para2.addClass("hide");
+  para2.addClass("hide");
+});
+
+$("#btntwo").on("click", function () {
+
+  para.addClass("show");
+});
+
+$("#btnthree").on("click", function () {
+
+  para.addClass("show");
+});
